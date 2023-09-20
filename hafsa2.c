0aac6af18aeeb3a2f5c259c705caddca3bd96aaf
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-09-20 12:04:48 +0100

    sd

---
+++ hafsa2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct TODO
+{
+    int id ,titre , desc , deadline, Status; 
+
+};
+int main(){
+
+struct TODO todo;
+printf("enter your id:");
+scanf("%d", &todo.id);
+
+
+
+}
+
